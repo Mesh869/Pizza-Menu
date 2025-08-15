2926741a9f60f64b33d025b6321956d6151926dc
David Meshack <meshuggar73@gmail.com>
2025-08-15 23:33:29 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+        <Pizza />
+      </div>
+    </>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Menu</h2>
+    </div>
+  );
+}
+
+export default App;
